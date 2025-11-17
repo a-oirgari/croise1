a64cda050e1191c803c9ee42d158408d17f57167
a-oirgari <abdrahmanghali1998@gmail.com>
2025-11-17 12:08:51 +0100

    file for the pop up and enhancement of the dynamique form

---
+++ js/modal.js
@@ -0,0 +1,81 @@
+
+const openAddModalBtn = document.getElementById('openAddModal');
+const addModal = document.getElementById('addModal');
+const cancelAdd = document.getElementById('cancelAdd');
+const photoInput = document.getElementById('photo');
+const previewImg = document.getElementById('preview');
+const addExpBtn = document.getElementById('addExp');
+const experiencesContainer = document.getElementById('experiences');
+const clearExpsBtn = document.getElementById('clearExps');
+
+openAddModalBtn.addEventListener('click', () => addModal.classList.remove('hidden'));
+cancelAdd.addEventListener('click', () => {
+  addModal.classList.add('hidden');
+  document.getElementById('addEmployeeForm').reset();
+  previewImg.src = '';
+  previewImg.classList.add('hidden');
+  experiencesContainer.innerHTML = '';
+});
+
+// Preview image 
+photoInput.addEventListener('input', (e) => {
+  const url = (e.target.value || '').trim();
+  if (!url) { previewImg.classList.add('hidden'); previewImg.src = ''; return; }
+  previewImg.src = url;
+  previewImg.classList.remove('hidden');
+});
+
+function createExperienceNode() {
+  const wrapper = document.createElement('div');
+  wrapper.className = 'border p-3 rounded-lg space-y-2 bg-gray-50 relative';
+
+  //Company
+  const company = document.createElement('input');
+  company.type = 'text';
+  company.placeholder = "Company";
+  company.className = 'w-full border p-2 rounded';
+
+  //Role
+  const role = document.createElement('input');
+  role.type = 'text';
+  role.placeholder = "Role";
+  role.className = 'w-full border p-2 rounded';
+
+  //Dates Row 
+  const datesRow = document.createElement('div');
+  datesRow.className = 'grid grid-cols-2 gap-2';
+
+  const from = document.createElement('input');
+  from.type = 'date';
+  from.placeholder = "From";
+  from.className = 'border p-2 rounded';
+
+  const to = document.createElement('input');
+  to.type = 'date';
+  to.placeholder = "To";
+  to.className = 'border p-2 rounded';
+
+  datesRow.appendChild(from);
+  datesRow.appendChild(to);
+
+  // Remove button
+  const removeBtn = document.createElement('button');
+  removeBtn.type = 'button';
+  removeBtn.className = 'absolute top-1 right-1 bg-red-300 text-red-800 px-2 py-0.5 rounded text-xs';
+  removeBtn.textContent = 'X';
+  removeBtn.addEventListener('click', () => wrapper.remove());
+
+  wrapper.appendChild(removeBtn);
+  wrapper.appendChild(company);
+  wrapper.appendChild(role);
+  wrapper.appendChild(datesRow);
+
+  return wrapper;
+}
+
+
+
+addExpBtn.addEventListener('click', () => {
+  experiencesContainer.appendChild(createExperienceNode());
+});
+clearExpsBtn.addEventListener('click', () => experiencesContainer.innerHTML = '');
